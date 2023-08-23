da7336ec5df3442f77bcb897d38c7d3c0307471c
johntzwei <johntzwei@yahoo.com>
2023-08-23 15:36:30 -0700

    pilot commit

---
+++ wikitext/pilot/convert_to_hf.sh
@@ -0,0 +1,1 @@
+python ./tools/convert_module_to_hf.py --input_dir ./wikitext_perturbed_cp/global_step1140/ --config_file haveibeentrainedon/wikitext/pilot/full_config.yml --output_dir haveibeentrainedon/wikitext/pilot/global_step1140
