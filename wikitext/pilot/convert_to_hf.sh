eb68625cffaeb17289a938c04057567e1c0202ba
johntzwei <johntzwei@yahoo.com>
2023-09-02 15:33:51 +0000

    clean commit for restructuring

--- wikitext/pilot/convert_to_hf.sh
+++ wikitext/pilot/convert_to_hf.sh
@@ -1,1 +1,1 @@
-python ./tools/convert_module_to_hf.py --input_dir ./wikitext_perturbed_cp/global_step1140/ --config_file haveibeentrainedon/wikitext/pilot/full_config.yml --output_dir haveibeentrainedon/wikitext/pilot/global_step1140
+python ./tools/convert_module_to_hf.py --input_dir ./wikitext_perturbed_cp/global_step228/ --config_file haveibeentrainedon/wikitext/pilot/full_config.yml --output_dir haveibeentrainedon/wikitext/pilot/global_step228
