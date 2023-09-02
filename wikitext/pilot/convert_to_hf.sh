ddaa42ee181f57cfafae32dbd3b1f8fa0e5d857f
johntzwei <johntzwei@yahoo.com>
2023-09-02 15:38:40 +0000

    moved everything out of the pile folder

--- wikitext/pilot/convert_to_hf.sh
+++
@@ -1,1 +0,0 @@
-python ./tools/convert_module_to_hf.py --input_dir ./wikitext_perturbed_cp/global_step228/ --config_file haveibeentrainedon/wikitext/pilot/full_config.yml --output_dir haveibeentrainedon/wikitext/pilot/global_step228
