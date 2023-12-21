e72cbb701f542bb24c4369c19655308f80b7a17e
johntzwei <johntzwei@yahoo.com>
2023-12-21 00:48:00 +0000

    seed fix

--- acl2024/unicode_subs/pipeline.sh
+++ acl2024/unicode_subs/pipeline.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#SBATCH --nodelist=glamor-ruby
+#SBATCH --nodelist=ink-noah
 #SBATCH --time=3-0:00
 #SBATCH --job-name=train
 #SBATCH --ntasks=16
 #SBATCH --gres=gpu:1
-#SBATCH --array=1-5
+#SBATCH --array=1-4
 #SBATCH --output=logs/slurm-%A_%a.out
+#SBATCH --qos=general
 
 eval "$(conda shell.bash hook)"
 conda activate gpt-neox
@@ -14,19 +15,19 @@ NEOX_DIR=/home/johnny/gpt-neox
 WORKING_DIR=$NEOX_DIR/haveibeentrainedon/acl2024/unicode_subs
 CUDA_HOME=$CONDA_PREFIX
 MODEL_YML=70M.yml
-EXP_NAME=replace_all_twenty
+EXP_NAME=sample_chars_70M
 
 frac=`echo "2 4 8 16 32" | cut -d' ' -f${SLURM_ARRAY_TASK_ID}`
 mkdir -p $WORKING_DIR/data/frac:$frac
 rm $WORKING_DIR/data/frac:$frac/*
 
-DS_PREFIX=$WORKING_DIR/data/frac:$frac/pile_perturbed
+DS_PREFIX=$WORKING_DIR/data/$EXP_NAME/frac:$frac/pile_perturbed
 papermill prepare.ipynb $WORKING_DIR/output.ipynb \
     -p frac_controlled 1 \
     -p frac_contaminated $frac \
     -p out_dataset_name $DS_PREFIX \
     -p out_samples_name $WORKING_DIR/data/frac:$frac/samples.csv \
-    -p strategy replace_all_twenty \
+    -p strategy sample_chars \
     -p debug False
 rm -r $DS_PREFIX.hf
 
@@ -40,7 +41,7 @@ python $NEOX_DIR/tools/preprocess_data.py \
     --append-eod \
     --workers 8
 
-MODEL_DIR=$WORKING_DIR/data/frac:$frac/70M
+MODEL_DIR=$WORKING_DIR/data/$EXP_NAME/frac:$frac/model
 rm -r $MODEL_DIR    # clean up
 
 cat $WORKING_DIR/configs/local_setup.yml | python create_yaml.py ${DS_PREFIX}_text_document $MODEL_DIR > $WORKING_DIR/configs/local_setup:$frac.yml
