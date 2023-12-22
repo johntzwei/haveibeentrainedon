76111252b4f82fd5be5281d60760a8d350846dff
Ryan Wang <ryanyxw@gmail.com>
2023-12-22 14:07:01 +0800

    encorporated pipeline for unstealthy raretoken experiments. Perturbations for raretoken experiments must be run inside their own directories

--- unstealthy/unstealthy_raretoken/perturb_raretoken.sh
+++ unstealthy/unstealthy_raretoken/perturb_raretoken.sh
@@ -1,6 +1,6 @@
-NEOX_DIR=./../gpt-neox
-DATA_DIR=./../data
-MODEL_DIR=./../models
+NEOX_DIR=./../../gpt-neox
+DATA_DIR=./../../data
+MODEL_DIR=./../../models
 
 watermark_length=20
 vocab_size=100
@@ -10,8 +10,8 @@ group_folder="run"
 new_dataset_name="pile1e8_20len"
 raw_dataset="${DATA_DIR}/pile1e8_orig.jsonl"
 repetitions="256"
-#the start of the vocabulary to which we are extracting random sequences
-start_range="0"
+#note that we will take the first 100 characters for each region
+extract_ranges="0 10000 20000 30000 40000 50000"
 
 #This exits the script if any command fails
 set -e
@@ -21,9 +21,9 @@ set -e
 #loop five times, each with different seed
 for i in {0..4}
 do
-  for repetition in $repetitions
+  for start_range in $extract_ranges
   do
-    temp_new_dataset_name="${new_dataset_name}_seed${i}/${repetition}_dataset"
+    temp_new_dataset_name="${new_dataset_name}_seed${i}/${start_range}_dataset"
     out_dir="${DATA_DIR}/${exp_name}/${group_folder}/${temp_new_dataset_name}"
 
     if [ -e $out_dir ]; then
@@ -33,7 +33,7 @@ do
 
     echo "beginning for ${temp_new_dataset_name}}"
 
-    python perturb_data.py\
+    python ./../perturb_data.py\
       --exp_name ${exp_name}\
       --raw_dataset ${raw_dataset}\
       --watermark_length ${watermark_length}\
@@ -41,7 +41,7 @@ do
       --out_dir ${out_dir}\
       --seed ${i}\
       --num_proc ${num_proc}\
-      --repetition ${repetition}\
+      --repetition ${repetitions}\
       --start_range ${start_range}
   done
 done
